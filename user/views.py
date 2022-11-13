454cb1836d928f0da477c7e35208c1ddf05f773c
EyimofeT <mofii4u@gmail.com>
2022-11-13 23:17:12 +0100

    PROTOTYPE V1-Speedpay-BE-TEST

---
+++ user/views.py
@@ -0,0 +1,209 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import UserSerializer, UserLoginSerializer, UserCreatedSerializer, UserDepositSerializer
+from .models import User
+from .serializers import UserSerializer
+from . import helpers
+import re
+from urllib import response
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+from rest_framework.exceptions import AuthenticationFailed
+# Create your views here.
+import json
+import jwt
+import datetime
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    list = {
+        "User SignUp": "/api/signup/",
+        "User LogIn": '/api/authentication/login/',
+        "User LogOut": '/api/authentication/logout/',
+        "User Deposit": "/api/userdeposit/",
+        "Users Withdrawal": '/api/withdrawal/',
+        "Users Account Details": '/api/accountdetails/',
+
+    }
+    return Response(list)
+
+
+@api_view(['POST'])
+def userCreate(request):
+    try:
+
+        # request.data['first_name']=request.data['first_name'].capitalize()
+        # request.data['last_name']=request.data['last_name'].capitalize()
+        request.data['firstname'] = request.data['firstname'].lower()
+        request.data['lastname'] = request.data['lastname'].lower()
+        request.data['username'] = request.data['username'].lower()
+        request.data['email'] = request.data['email'].lower()
+        request.data
+        # print(request.data)
+        serializer = UserSerializer(data=request.data)
+        request.data['balance'] = float(0)
+        request.data['account_number'] = helpers.generate_account_number()
+        uname = str(request.data['username'])
+        email = str(request.data['email'])
+        uniqueUsernameStatus = helpers.checkUsernameInput(uname)
+        uniqueEmailStatus = helpers.checkEmailInput(email)
+        # print("here")
+        # print(uniqueEmailStatus)
+        if uniqueUsernameStatus == True and uniqueEmailStatus == True:
+            try:
+                print("i am here")
+                if serializer.is_valid():
+                    print("i am here1")
+                    serializer.save()
+                    print("i am here2")
+                    user = User.objects.all().last()
+                    serializer = UserCreatedSerializer(user)
+
+                    response = {
+                        "Status": "200",
+                        "data": [
+                            serializer.data
+                        ]
+                    }
+                else:
+                    response = {
+                        "Error: User Not Created : Check Input"
+                    }
+            except Exception as e:
+                response = {"Something went wrong": e}
+        elif uniqueUsernameStatus == False and uniqueEmailStatus == True:
+            response = {
+                "Error: Username Already Exists"
+            }
+        elif uniqueUsernameStatus == True and uniqueEmailStatus == False:
+            response = {
+                "Error: Email Already Exists"
+            }
+        elif uniqueUsernameStatus == False and uniqueEmailStatus == False:
+            response = {
+                "Error: Username and Email Already Exists"
+            }
+
+    except Exception as e:  # work on python 2.x
+        # logger.error('Failed to upload to ftp: '+ str(e))
+        response = {
+            "Error Occured": str(e)
+        }
+
+    return Response(response)
+
+
+@api_view(['GET'])
+def accountdetails(request):
+    token = request.COOKIES.get('jwt')
+    if not token:
+        raise AuthenticationFailed('Unauthenticated')
+
+    try:
+
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed('Unauthorized')
+
+    user = User.objects.filter(id=payload['id']).first()
+    # user = User.objects.get(id=payload['id'])
+    serializer = UserCreatedSerializer(user)
+
+    return Response(serializer.data)
+    # return Response(token)
+
+
+@api_view(['POST'])
+def deposit(request):
+    token = request.COOKIES.get('jwt')
+    if not token:
+        raise AuthenticationFailed('Unauthenticated')
+
+    try:
+
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+        user = User.objects.get(id=payload["id"])
+        if (float(request.data['amount']) > 0):
+            current_balance = user.balance
+            new_balance = current_balance + float(request.data['amount'])
+            user.balance = new_balance
+
+            user.save()
+            response = {
+                "status": 200,
+                "message": "Deposit Successful"
+            }
+        else:
+            response = {
+
+                "status": 400,
+                "message": "Invalid Amount "
+            }
+
+    except KeyError:
+        response = {
+            "status": 400,
+            "message": "Missing Amount"
+        }
+    except ValueError:
+        response = {
+            "status": 400,
+            "message": "Invalid Amount"
+        }
+
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed('Unauthorized')
+
+    # users = User.objects.get(id=pk)
+    return Response(response)
+
+
+@api_view(['POST'])
+def withdrawal(request):
+    token = request.COOKIES.get('jwt')
+    if not token:
+        raise AuthenticationFailed('Unauthenticated')
+
+    try:
+
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+        user = User.objects.get(id=payload["id"])
+        current_balance = user.balance
+        if (current_balance >= float(request.data['amount'])):
+            new_balance = current_balance - float(request.data['amount'])
+            user.balance = new_balance
+
+            user.save()
+            response = {
+                "status": "200",
+                "message": "Withdrawal Successful"
+            }
+        else:
+            response = {
+                "status": "400",
+                "message": "Insufficient Funds"
+            }
+
+    except KeyError:
+        response = {
+            "status": "400",
+            "message": "Missing Amount"
+        }
+    except ValueError:
+        response = {
+            "status": "400",
+            "message": "Invalid Amount"
+        }
+
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed('Unauthorized')
+
+    # users = User.objects.get(id=pk)
+    return Response(response)
